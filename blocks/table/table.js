7928fbc6b9f92f53fec51b34193e31434ccf64c4
swati <cof32845@adobe.com>
2024-11-23 17:55:35 +0530

    Training Assignments

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,4 +1,62 @@
 
+const itemsPerPage = 5;
+let currentPage = 1;
+function renderList(studentdata) {
+  const studentList = document.createElement('div');
+  const studentListData = document.createElement('ul');
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = currentPage * itemsPerPage;
+  const itemsToDisplay = studentdata.slice(startIndex, endIndex);
+  console.log(itemsToDisplay);
+
+  // Clear The Current List 
+  studentListData.innerHTML = '';
+
+  itemsToDisplay.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `Student Name: ${item.name}, Gender: ${item.gender}`;
+    studentListData.appendChild(listItem);
+  });
+  const paginationList = updatePagination(studentdata);
+  studentList.appendChild(studentListData);
+ studentList.appendChild(paginationList);
+
+  console.log(studentList);
+  return studentList;
+}
+function updatePagination(studentdata) {
+  const pagination = document.createElement('div');
+  pagination.classList.add('pagination');
+  pagination.innerHTML = '';
+  // Create Previous Button
+
+  const prevBtn = document.createElement('button');
+  prevBtn.textContent = 'Prev';
+  prevBtn.disabled = currentPage === 1;
+  prevBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      renderList(studentdata);
+    }
+  });
+  // Create Next Button
+
+  const nextBtn = document.createElement('button');
+  nextBtn.textContent = 'Next';
+  nextBtn.disabled = currentPage * itemsPerPage >= studentdata.length;
+  nextBtn.addEventListener('click', () => {
+    if (currentPage * itemsPerPage < studentdata.length) {
+      currentPage++;
+      console.log("Current Page Value", currentPage);
+      renderList(studentdata);
+    }
+  });
+  //Append buttons to the pagination container
+
+  pagination.appendChild(prevBtn);
+  pagination.appendChild(nextBtn);
+  return pagination;
+}
 async function createList(jsonURL, val) {
   let pathname = null;
   if (val) {
@@ -10,23 +68,9 @@ async function createList(jsonURL, val) {
   const resp = await fetch(pathname);
   const json = await resp.json();
   const studentdata = json.data;
-  console.log("json data", studentdata);
-  const studentList = document.createElement('div');
-  const studentListData = document.createElement('ul');
-  const pagination = document.createElement('div');
-  pagination.classList.add('pagination');
-  const itemsPerPage = 5;
-  let currentPage = 1;
+  const listData = renderList(studentdata);
+  return listData;
 
-  studentdata.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `Student Name: ${item.name} Gender: ${item.gender}`;
-    studentListData.appendChild(listItem);
-  });
-  studentList.appendChild(studentListData);
-  studentList.appendChild(pagination);
- 
-  return studentList;
 }
 export default async function decorate(block) {
   const students = block.querySelector('a[href$=".json"]');
@@ -34,6 +78,7 @@ export default async function decorate(block) {
   parentDiv.classList.add('student-block');
   if (students) {
     parentDiv.append(await createList(students.href, null));
+    console.log(parentDiv);
     students.replaceWith(parentDiv);
   }
 }
